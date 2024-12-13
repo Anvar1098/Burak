864b45f0c15ff89d21f200cd4baf7c7e955411d6
Anvar1098 <anvar00aminov@gmail.com>
2024-12-13 17:46:47 +0900

    feat: develop signup process

--- standards.ts
+++ standards.ts
@@ -7,3 +7,8 @@
  css => Snake                            buton_style
 
 */
+
+/**MONGO_URL= "mongodb+srv://anvar00:CZ2CVKkprEreoL2W@cluster0.wetuy.mongodb.net/Burak 
+ * 
+ * mongodb+srv://anvar00:CZ2CVKkprEreoL2W@cluster0.wetuy.mongodb.net/Burak?retryWrites=true&w=majority&appName=Cluster0
+*/
