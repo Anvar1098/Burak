eb4fddad01d4e47db061bdb3ff1e4eb5182e8b99
0000 <a0000@Anvars-MacBook-Air.local>
2025-02-12 17:48:08 +0900

    fix: updating

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,6 +1,6 @@
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
-import  { Request, Response } from "express";
+import { Request, Response } from "express";
 import ProductService from "../models/Product.service";
 import { ProductInput, ProductInquiry } from "../libs/types/product";
 import { AdminRequest, ExtendedRequest } from "../libs/types/member";
@@ -12,7 +12,7 @@ const productController: T = {};
 
 /** SPA **/
 
-productController.getProducts = async (req:Request, res: Response) => {
+productController.getProducts = async (req: Request, res: Response) => {
     try {
         console.log('getProducts');
         const { page, limit, order, productCollection, search } = req.query;
@@ -21,19 +21,19 @@ productController.getProducts = async (req:Request, res: Response) => {
             page: Number(page),
             limit: Number(limit),
         };
-        if(productCollection) {
+        if (productCollection) {
             inquiry.productCollection = productCollection as ProductCollection;
         }
         if (search) inquiry.search = String(search);
-    
+
         const result = await productService.getProducts(inquiry);
 
-       res.status(HttpCode.OK).json(result);
+        res.status(HttpCode.OK).json(result);
     } catch (err) {
         console.log('Error, getProducts:', err);
         if (err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standard.code).json(Errors.standard);
-    } 
+    }
 };
 
 productController.getProduct = async (req: ExtendedRequest, res: Response) => {
@@ -43,19 +43,19 @@ productController.getProduct = async (req: ExtendedRequest, res: Response) => {
         const { id } = req.params;
         const memberId = req.member?._id ?? null,
             result = await productService.getProduct(memberId, id);
-       
+
 
         res.status(HttpCode.OK).json(result);
     } catch (err) {
         console.log('Error, getProduct:', err);
         if (err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standard.code).json(Errors.standard);
-    } 
+    }
 };
 
 /** SSR **/
 
-productController.getAllproducts =  async (req: Request, res: Response) => {
+productController.getAllproducts = async (req: Request, res: Response) => {
     try {
         console.log('getAllproducts');
         const data = await productService.getAllProducts();
@@ -65,14 +65,14 @@ productController.getAllproducts =  async (req: Request, res: Response) => {
         console.log('Error, getAllproducts:', err);
         if (err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standard.code).json(Errors.standard);
-    } 
+    }
 };
 
 productController.createNewProduct = async (req: AdminRequest, res: Response) => {
     try {
         console.log("createNewProduct");
-        
-        if(!req.files?.length)
+
+        if (!req.files?.length)
             throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
 
         const data: ProductInput = req.body;
@@ -80,7 +80,7 @@ productController.createNewProduct = async (req: AdminRequest, res: Response) =>
             return ele.path.replace(/\\/g, '/');
         });
 
-        await productService.createNewProduct(data); 
+        await productService.createNewProduct(data);
 
         res.send(`
             <script>  alert("Successful Creation");
@@ -93,22 +93,22 @@ productController.createNewProduct = async (req: AdminRequest, res: Response) =>
             <script>  alert("${message}");
             window.location.replace('/admin/product/all') </script>}`
         );
-     }
+    }
 };
 
-productController.updateChosenProduct =  async (req: Request, res: Response) => {
+productController.updateChosenProduct = async (req: Request, res: Response) => {
     try {
         console.log('updateChosenProduct');
         const id = req.params.id;
-        
+
         const result = await productService.updateChosenProduct(id, req.body);
-        
+
         res.status(HttpCode.OK).json({ data: result });
     } catch (err) {
         console.log('Error, updateChosenProduct:', err);
         if (err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standard.code).json(Errors.standard);
-    } 
+    }
 };
 
 export default productController;
